4823adbb0363eb4e1e47ae000f937cd45bb3acc9
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-01-08 12:39:05 +0100

    content update

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,23 +1,21 @@
-    const slide = n => {
+    const slide = section => {
         if (slideInProgress === false) {
-            document.querySelector("#container").style.marginTop = `-${sectionHeights[n]}px`;
-            currentSection = n;
+            document.querySelector("#container").style.marginTop = `-${sectionHeights[section]}px`;
+            currentSection = section;
             slideInProgress = true;
             setTimeout(function() { slideInProgress = false }, 1100);
         }
     }
-    const slideWheel = event => {
+    const slideOnScroll = scrollEvent => {
         if (slideInProgress === false)
-            if (event.deltaY > 0 && currentSection + 1 < sections.length) slide(currentSection + 1);
-            else if (event.deltaY < 0 && currentSection > 0) slide(currentSection - 1);
-
+            if (scrollEvent.deltaY > 0 && currentSection + 1 < sections.length) slide(currentSection + 1);
+            else if (scrollEvent.deltaY < 0 && currentSection > 0) slide(currentSection - 1);
     }
 
     document.querySelector("#btn-purple").addEventListener("click", function() { slide(1) });
-    document.querySelector("#btn-red").addEventListener("click", function() { slide(0) });
     document.querySelector(".back").addEventListener("click", function() { window.open("https://digital24.pl", "_self") });
     document.addEventListener("load", function() { window.scrollTo(0, 0) });
-    window.addEventListener("wheel", slideWheel);
+    window.addEventListener("wheel", slideOnScroll);
 
     slideInProgress = false;
     let sections = document.querySelectorAll(".section");

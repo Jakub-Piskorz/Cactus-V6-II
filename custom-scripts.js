f28d93c9aaf94fdf9a2c72a4ca10629353994f8b
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-01-08 14:48:59 +0100

    hotfix

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,6 +1,6 @@
-    const slide = section => {
-        if (slideInProgress === false) {
-            document.querySelector("#container").style.marginTop = `-${sectionHeights[section]}px`;
+    const slide = (section, forced = false) => {
+        if (slideInProgress === false || forced === true) {
+            document.querySelector("#container").style.marginTop = `-${sectionLocations[section]}px`;
             currentSection = section;
             slideInProgress = true;
             setTimeout(function() { slideInProgress = false }, 900);
@@ -11,18 +11,22 @@
             if (scrollEvent.deltaY > 0 && currentSection + 1 < sections.length) slide(currentSection + 1);
             else if (scrollEvent.deltaY < 0 && currentSection > 0) slide(currentSection - 1);
     }
-
-    document.querySelector("#btn-purple").addEventListener("click", function() { slide(1) });
-    document.querySelector(".back").addEventListener("click", function() { window.open("https://digital24.pl", "_self") });
-    document.addEventListener("load", function() { window.scrollTo(0, 0) });
-    window.addEventListener("wheel", slideOnScroll);
+    const updateLocations = (forced = false) => {
+        sectionLocations = [0];
+        sections.forEach((x, i) => sectionLocations.push(sections[i].clientHeight + sectionLocations[i]));
+        slide(currentSection, forced);
+    }
 
     slideInProgress = false;
     let sections = document.querySelectorAll(".section");
-    let sectionHeights = [0];
-    sections.forEach((x, i) => sectionHeights.push(sections[i].clientHeight + sectionHeights[i]));
+    let sectionLocations = [];
     let currentSection = 0;
 
+    document.querySelector("#btn-purple").addEventListener("click", function() { slide(1) });
+    document.querySelector(".back").addEventListener("click", function() { window.open("https://digital24.pl", "_self") });
+    window.addEventListener("wheel", slideOnScroll);
+    window.onresize = function() { updateLocations(true) };
+    updateLocations();
 
 
     //setInterval(function() { console.log(slideInProgress) }, 100);

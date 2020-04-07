ad3f9f22dd28f2faf45e7885cf36b69d223e1d92
Jakub Piskorz <kontakt@jakubpiskorz.pl>
2020-04-07 16:09:24 +0100

    fixing code readability

--- modules.js
+++ modules.js
@@ -1,20 +1,27 @@
-    const slide = (section, attr) => {
-        if ((slideInProgress === false || attr === "forced") && !RWD) {
-            document.querySelector("#container").style.marginTop = `-${sectionLocations[section]}px`;
-            currentSection = section;
-            if (attr !== "forced") {
-                slideInProgress = true;
-                setTimeout(function() { slideInProgress = false }, 1100);
+    const slide = (() => {
+        {
+            let slideInProgress = false;
+            return (section, attr) => {
+                if ((slideInProgress === false || attr === "forced") && !RWD) {
+                    document.querySelector("#container").style.marginTop = `-${sectionLocations[section]}px`;
+                    currentSection = section;
+                    if (attr !== "forced") {
+                        slideInProgress = true;
+                        setTimeout(function () {
+                            slideInProgress = false
+                        }, 1100);
+                    }
+                    updateNav(section);
+                } else if (attr === "click") document.querySelector("#main").scrollBy(0, 700);
             }
-            updateNav(section);
-        } else if (attr === "click") document.querySelector("#main").scrollBy(0, 700);
-    }
+        }
+    })();
     const updateNav = (section = currentSection, primaryColor = "black", restColor = "#d7000f") => {
         for (let n of document.querySelectorAll("nav a")) n.style.color = primaryColor;
         document.querySelectorAll("nav a")[section + 1].style.color = restColor;
     }
     const slideOnScroll = () => {
-        if (slideInProgress === false && RWD === false)
+        if (RWD === false)
             if (event.deltaY >= 2 && currentSection + 1 < sections.length) slide(currentSection + 1);
             else if (event.deltaY <= -2 && currentSection > 0) slide(currentSection - 1);
         console.log(event.deltaY);
@@ -35,7 +42,7 @@
         if (RWD === false && (window.innerWidth < 900 || window.innerHeight < 810)) {
             RWD = true;
             document.querySelector("#container").style.marginTop = 0;
-            document.querySelector("#main").scrollTo(0,0);
+            document.querySelector("#main").scrollTo(0, 0);
         }
     }
     const nextSlideButton = (color = "black", toggle = "show", delay = 0) => {
@@ -72,20 +79,20 @@
         }, delay + 700);
     }
 
-    let slideInProgress = false;
     let nextSlideButtonCalls = 0;
     let sections = document.querySelectorAll(".section");
     let sectionLocations = [];
     let currentSection = 0;
     let RWD = window.innerWidth < 900 ? true : false;
 
-    if (document.querySelector("#slide-marker")) document.querySelector("#slide-marker").addEventListener("click", event => slide(currentSection + 1));
-    if (document.querySelector(".link")) document.querySelector(".link").addEventListener("click", event => window.open("https://digital24.pl/wyzwalanie-lamp/cactus-v6-ii-bezprzewodowy-wyzwalacz-do-lamp-blyskowych.html", "_self"));
-    if (document.querySelector("#btn-red")) document.querySelector("#btn-red").addEventListener("click", event => slide(currentSection + 1, "click"));
-
+    //Customizing script for specific page
     window.addEventListener("wheel", slideOnScroll);
     window.onresize = event => {
         switchRwd();
         updateLocations();
     };
+    if (document.querySelector("#slide-marker")) document.querySelector("#slide-marker").addEventListener("click", event => slide(currentSection + 1));
+    if (document.querySelector(".link")) document.querySelector(".link").addEventListener("click", event => window.open("https://digital24.pl/wyzwalanie-lamp/cactus-v6-ii-bezprzewodowy-wyzwalacz-do-lamp-blyskowych.html", "_self"));
+    if (document.querySelector("#btn-red")) document.querySelector("#btn-red").addEventListener("click", event => slide(currentSection + 1, "click"));
+
     updateLocations("forced");
